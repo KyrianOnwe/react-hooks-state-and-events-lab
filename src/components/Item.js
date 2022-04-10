bbfb673a71c82e31594f0faf629daff34aa8d0b6
Kyrian <dr.kyrian.onwe@gmail.com>
2022-04-09 21:55:00 -0500

    commit tree 5d6d4167c4eee0ec278707e72b5ed2b8aed27c1b

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [status, setStatus] = useState(false); 
+
+  function switchStatus(){
+    setStatus(() => !status)
+
+  }
+
+  const inCart = status ? "in-cart" : "";
+
+  const addOrRemove = !status ? "Add to Cart" : "Remove From Cart"
+
   return (
-    <li className="">
+    <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={switchStatus}>{addOrRemove}</button>
     </li>
   );
 }
